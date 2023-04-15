d4033aeb008f6a479d82542ee23864a98f540f52
nick <divanchmo@gmail.com>
2023-04-15 22:54:09 +0300

    init commit

---
+++ sem1/Test.java
@@ -0,0 +1,47 @@
+package jjjava.sem1;
+
+/**
+ * test
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        /*
+        //task2
+        
+        int[] mas = {1, 1, 0, 1, 1, 1, 0, 1, 1};
+        int countmax = 0;
+        int count = 0;
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] == 1) {
+                count++;
+                if (count > countmax) countmax = count;
+            }
+            else count = 0;
+            }
+            System.out.println(countmax);
+        */
+
+        /*
+        //task3
+        
+        int[] mas = {3,2,2,3,1,4,2,5,3,1,2};
+        int val = 3;
+        int temp;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length; j++) {
+                if (mas[j] == val && j < mas.length - 1) {
+                    temp = mas[j];
+                    mas[j] = mas[j+1];
+                    mas[j+1] = temp;
+                }
+            } 
+        }
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+        }
+        */
+
+
+    }
+}
